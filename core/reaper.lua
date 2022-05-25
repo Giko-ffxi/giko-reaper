94a7a828a4e495e031af3e31fe183dc02ea82250
Giko <giko.ffxi@gmail.com>
2022-05-25 12:50:57 -0400

    Removed unused code

--- core/reaper.lua
+++ core/reaper.lua
@@ -3,33 +3,9 @@ local cache    = require('lib.giko.cache')
 local common   = require('lib.giko.common')
 local chat     = require('lib.giko.chat')
 local monster  = require('lib.giko.monster')
-local watcher  = { cache = string.format('%s\\..\\giko-cache\\cache\\giko.monster.id.csv', _addon.path) }
-local ids      = {}
+local reaper   = { }
 
-watcher.load  = function()
-
-    ids = cache.get_all(watcher.cache)
-    
-end
-
-watcher.watch = function(mode, input, m_mode, m_message, blocked)
-    
-    local target = GetEntity(AshitaCore:GetDataManager():GetTarget():GetTargetIndex())
-
-    if (target ~= nil) then
-        for key,mob in ipairs(monster.notorious) do
-            for n,name in ipairs(common.flatten(mob.names)) do  
-                if string.lower(target.Name) == string.lower(name) and target.SpawnFlags == 0x0010 and not common.in_array_key(ids, string.lower(target.Name)) then
-                    ids[string.lower(target.Name)] = target.ServerId
-                    cache.set(watcher.cache, k, i)
-                end
-            end
-        end
-    end
-
-end
-
-watcher.listen = function(id, size, packet, packet_modified, blocked)
+reaper.listen = function(id, size, packet, packet_modified, blocked)
     
     if (id == 0x38) then
 
@@ -52,4 +28,4 @@ watcher.listen = function(id, size, packet, packet_modified, blocked)
 
 end
 
-return watcher
+return reaper
--- giko-reaper.lua
+++ giko-reaper.lua
@@ -4,8 +4,6 @@ _addon.author 	= 'giko'
 _addon.name 	= 'giko-reaper'
 _addon.version 	= '1.0.0'
 
-watcher = require('core.watcher')
+reaper = require('core.reaper')
 
-ashita.register_event('load', watcher.load)
-ashita.register_event('render', watcher.watch)
-ashita.register_event('incoming_packet', watcher.listen)
+ashita.register_event('incoming_packet', reaper.listen)
