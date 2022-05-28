ed7485bc126390426b8f4109820852e050cde14f
Giko <giko.ffxi@gmail.com>
2022-05-28 13:53:49 -0400

    Update

--- core/reaper.lua
+++ core/reaper.lua
@@ -13,11 +13,11 @@ reaper.listen = function(id, size, packet, packet_modified, blocked)
         local zone     = AshitaCore:GetResourceManager():GetString('areas', AshitaCore:GetDataManager():GetParty():GetMemberZone(0));
         local deceased = GetEntity(index)
 
-        if (deceased ~= nil and string.find(string.lower(zone), "nyzul") == nil and string.find(string.lower(zone), "riverne") == nil then
+        if deceased ~= nil and string.find(string.lower(zone), "nyzul") == nil and string.find(string.lower(zone), "riverne") == nil then
             for key,mob in ipairs(monster.notorious) do
                 for n,name in ipairs(common.flatten(mob.names)) do
                     if string.lower(deceased.Name) == string.lower(name) and deceased.SpawnFlags == 0x0010 then
-                        chat.tell(config.broadcaster, string.format('@giko set-tod %s now', string.lower(string.gsub(deceased.Name, ' ', '-'))))
+                        ashita.timer.create(string.format('reaper-%s', string.lower(name)), config.delay, 1, function() chat.tell(config.broadcaster, string.format('@giko set-tod %s now', string.lower(string.gsub(deceased.Name, ' ', '-')))) end)
                     end
                 end
             end
