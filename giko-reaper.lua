eb3df3d4cc8f8684ddfb76706d05007490145032
Giko <giko.ffxi@gmail.com>
2022-10-01 23:58:57 -0400

    Version 1.05

--- giko-reaper.lua
+++ giko-reaper.lua
@@ -2,8 +2,9 @@ package.path = (string.gsub(_addon.path, '[^\\]+\\?$', '')) .. 'giko-common\\' .
 
 _addon.author 	= 'giko'
 _addon.name 	= 'giko-reaper'
-_addon.version 	= '1.0.0'
+_addon.version 	= '1.0.5'
 
 reaper = require('core.reaper')
 
 ashita.register_event('incoming_packet', reaper.listen)
+
