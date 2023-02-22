39285efc0846ad3903fb44b49472d0f8b69ae968
Giko <giko.ffxi@gmail.com>
2023-02-21 19:31:18 -0500

    1.0.6

--- core/reaper.lua
+++ core/reaper.lua
@@ -19,7 +19,8 @@ reaper.listen = function(id, size, packet, packet_modified, blocked)
                 for n,name in ipairs(common.flatten(mob.names)) do
                     if string.lower(deceased.Name) == string.lower(name) and deceased.SpawnFlags == 0x0010 then
                         if config.monsters[mob.names.nq[1]] ~= nil and config.monsters[mob.names.nq[1]].broadcast == false then
-                            death.set_tod(mob.names.nq[1], os.date('%Y-%m-%d %H:%M:%S', os.time() - common.offset_to_seconds(os.date('%z', os.time()))))                          
+                            death.set_tod(mob.names.nq[1], os.date('%Y-%m-%d %H:%M:%S', os.time() - common.offset_to_seconds(os.date('%z', os.time())))) 
+                            chat.command('giko timer purge')
                         else
                             ashita.timer.create(string.format('reaper-%s', string.lower(name)), config.delay or 3, 1, function() chat.tell(config.broadcaster, string.format('@giko set-tod %s now', string.lower(string.gsub(deceased.Name, ' ', '-')))) end)
                         end
--- giko-reaper.lua
+++ giko-reaper.lua
@@ -2,9 +2,8 @@ package.path = (string.gsub(_addon.path, '[^\\]+\\?$', '')) .. 'giko-common\\' .
 
 _addon.author 	= 'giko'
 _addon.name 	= 'giko-reaper'
-_addon.version 	= '1.0.5'
+_addon.version 	= '1.0.6'
 
 reaper = require('core.reaper')
 
 ashita.register_event('incoming_packet', reaper.listen)
-
